b43455c0730f7380eab6eab39018564386d3d4a5
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-03-02 17:42:13 +0200

    test go build

--- ch1/helloworld.go
+++ ch1/helloworld.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 输出") // 输出 Hello, Go, natively handles UTF-8, so you can directly include Chinese characters in your strings.
+	fmt.Println("Hello, 输出")                // 输出 Hello, Go, natively handles UTF-8, so you can directly include Chinese characters in your strings.
+	fmt.Println("Hello,", "Kwaillon", "输出") // 输出 Hello, Kwaillon 输出
 }
