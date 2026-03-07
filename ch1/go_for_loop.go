452f5f42ffd56d8066fb55aeb95aa90b24221e6f
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-03-07 14:55:46 +0200

    fix: add go_for_loop.go

---
+++ ch1/go_for_loop.go
@@ -0,0 +1,26 @@
+//go:build ignore
+
+// This program demonstrates the use of for loops in Go to print command-line arguments.
+
+package main
+
+func main() {
+	/**
+	The "for" loop in Go, the only loop construct in Go, can be used in three different ways:
+
+	1. Traditional for loop with initialization, condition, and post statement:
+	for initialisation; condition; post {
+		// loop body
+	}
+
+	2. The "while" style loop, where only the condition is specified:
+	for condition {
+		// loop body
+	}
+
+	3. The "infinite" loop, where no condition is specified:
+	for {
+		// loop body
+	}
+	*/
+}
