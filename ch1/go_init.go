4bf3587a6173b34d55f6ad05ebdc394d0e67c883
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-03-07 15:36:55 +0200

    fix: add different ways to init a variable

---
+++ ch1/go_init.go
@@ -0,0 +1,22 @@
+//go:build ignore
+
+// This program demonstrates different ways to initialise.
+
+package main
+
+func main() {
+	/**
+
+	There are 4 different ways to initialise a variable in Go:
+
+	1. s:= ""
+	2. var s string
+	3. var s = ""
+	4. var s string = ""
+
+		- 1 and 2 should be the most commonly used.
+		- explicit initialisation is for important initialisation
+		- implicit initialisation is for unimportant initialisation
+
+	*/
+}
