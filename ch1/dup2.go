bf8bfe85cbc38288e3afe5c06ea5953e3847bb68
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-03-11 02:53:45 +0200

    add dup2.go

---
+++ ch1/dup2.go
@@ -0,0 +1,39 @@
+//go:build ignore
+
+/**
+ * This program prints a duplicated line preceded by its countl, expansion of dup1.go
+ * Usage: Use with stdin or with a list of named files.
+ * UNFINISHED, NOT WORKING YET
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+
+	} else {
+
+	}
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+
+}
