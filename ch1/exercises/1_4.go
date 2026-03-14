5714e3817172372d263c03d64042f0d91d6b21f9
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-03-14 05:06:59 +0200

    fix 1_4

--- ch1/exercises/1_4.go
+++ ch1/exercises/1_4.go
@@ -1,5 +1,10 @@
 //go:build ignore
 
+/**
+ * This program prints a duplicated line preceded by its count and the names of all files in which it appears.
+ * Usage: Use with a list of named files.
+ */
+
 package main
 
 import (
@@ -7,36 +12,38 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	allDupFiles := ""
-	where := make(map[string]map[string]int)
-	for _, file := range os.Args[1:] {
-		count := make(map[string]int)
-		b, err := os.ReadFile(file)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dup4 error: %v\n", err)
-			continue
-		}
-		scanner := bufio.NewScanner(bytes.NewReader(b))
-		for scanner.Scan() {
-			count[scanner.Text()]++
-		}
-		for _, dupCount := range count {
-			if dupCount > 1 {
-				where[file] = count
+	counts := make(map[string]int)
+	where := make(map[string]string)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		fmt.Fprintf(os.Stderr, "dup: no files\n")
+		os.Exit(1)
+	} else {
+		for _, file := range files {
+			b, err := os.ReadFile(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup err: %v\n", err)
+				continue
+			}
+			input := bufio.NewScanner(bytes.NewReader(b))
+			for input.Scan() {
+				counts[input.Text()]++
+				if where[input.Text()] == "" {
+					where[input.Text()] = file
+				} else if !strings.Contains(where[input.Text()], file) {
+					where[input.Text()] += ", " + file
+				}
 			}
 		}
 	}
-	for file := range where {
-		allDupFiles += file + " "
-	}
-	for _, countMap := range where {
-		for line, count := range countMap {
-			if count > 1 {
-				fmt.Printf("%d %s\t(%s)\n", count, line, allDupFiles)
-			}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\t\t%s\n", count, line, where[line])
 		}
 	}
 }
